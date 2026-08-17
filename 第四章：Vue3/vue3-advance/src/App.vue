2b00cc1f120b97f0ad2a6dbc28186920dad30d41
tanpeng <tanpeng@bjgoodwill.com>
2026-08-17 16:10:01 +0800

    自定义组件

--- 第四章：Vue3/vue3-advance/src/App.vue
+++ 第四章：Vue3/vue3-advance/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import MouseDemo from './components/MouseDemo.vue'
+import DirectivesDemo from './components/DirectivesDemo.vue'
 </script>
 
 <template>
-  <MouseDemo />
+  <!-- <MouseDemo /> -->
+  <DirectivesDemo />
 </template>
 
 <style scoped></style>
--- 第四章：Vue3/vue3-advance/src/main.js
+++ 第四章：Vue3/vue3-advance/src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+// 全局注册自定义指令
+app.directive('focus', {
+    mounted(el, binding, vnode) {
+        el.focus()
+        el.style.backgroundColor = 'red'
+    },
+    // 绑定元素的父组件更新前调用
+    beforeUpdate(el, binding, vnode, prevVnode) {
+        el.style.backgroundColor = 'blue'
+    }
+
+})
+
+app.mount('#app')

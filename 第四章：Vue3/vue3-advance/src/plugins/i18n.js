e0af90adcd756143a71f408da4d803e811a4bcfd
tanpeng <tanpeng@bjgoodwill.com>
2026-08-17 16:41:05 +0800

    i18n

--- 第四章：Vue3/vue3-advance/src/plugins/i18n.js
+++ 第四章：Vue3/vue3-advance/src/plugins/i18n.js
@@ -1,10 +1,9 @@
 export default {
     install: (app, options) => {
-        // 全局注册 $t 方法，用于在模板中调用 i18n 实例的 t 方法
-        app.config.globalProperties.$t = (key) => {
-            // 递归查找 key 中的每个属性，返回最终的值
-            // 如果 key 中的属性不存在，返回 key 本身
-            // 例如：'hello.world' 会返回 'hello.world'
+        // 注入一个全局可用的 $translate() 方法
+        app.config.globalProperties.$translate = (key) => {
+            // 获取 `options` 对象的深层属性
+            // 使用 `key` 作为索引
             return key.split('.').reduce((o, i) => {
                 if (o) {
                     return o[i]

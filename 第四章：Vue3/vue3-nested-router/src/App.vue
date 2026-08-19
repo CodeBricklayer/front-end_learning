cf81fcc78bebdd30db3bd8f91f4a7dc9eedf29fc
tanpeng <tanpeng@bjgoodwill.com>
2026-08-19 10:52:12 +0800

    编程式导航

--- 第四章：Vue3/vue3-nested-router/src/App.vue
+++ 第四章：Vue3/vue3-nested-router/src/App.vue
@@ -1,4 +1,40 @@
-<script setup></script>
+<script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const goToPage1 = () => {
+  // router.push('/list')
+  router.push({
+    path: '/list'
+  })
+}
+
+const goToPage2 = () => {
+
+  router.push({
+    name: 'userView',
+    params: {
+      name: 'baidu'
+    }
+  })
+}
+
+const goBack = () => {
+  // 后退
+  router.go(-1)
+}
+
+const goForward = () => {
+  // 前进
+  router.go(1)
+}
+
+const goReplace = () => {
+  // 替换
+  router.replace('/list')
+}
+</script>
 
 <template>
   <ul>
@@ -11,8 +47,17 @@
   <ul>
     <li><router-link to="/">首页</router-link></li>
     <!-- 动态路由参数 -->
-    <li><router-link :to="{name:'userView',params:{name:'baidu'}}">去百度用户新</router-link></li>
+    <li><router-link :to="{ name: 'userView', params: { name: 'baidu' } }">去百度用户新</router-link></li>
   </ul>
+
+  <div>
+    <h3>这是编程式导航</h3>
+    <button @click="goToPage1">按钮1</button>
+    <button @click="goToPage2">按钮2</button>
+    <button @click="goBack">后退</button>
+    <button @click="goForward">前进</button>
+    <button @click="goReplace">替换</button>
+  </div>
   <router-view></router-view>
 </template>
 
